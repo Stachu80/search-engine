1efd8e2c9707abbc7b2d1cd69159fe10a05178f4
Stachu80 <michal.stasinski80@gmail.com>
2017-11-18 17:36:53 +0100

    dao

--- src/main/java/com/infoshareacademy/searchengine/Main.java
+++ src/main/java/com/infoshareacademy/searchengine/Main.java
@@ -1,7 +1,49 @@
 package com.infoshareacademy.searchengine;
 
+import com.infoshareacademy.searchengine.dao.UsersRepository;
+import com.infoshareacademy.searchengine.dao.UsersRepositoryDao;
+import com.infoshareacademy.searchengine.dao.UsersRepositoryDaoBean;
+import com.infoshareacademy.searchengine.domain.User;
+
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Heloo World");
+    public static void main(String[] arogs) {
+
+
+
+        UsersRepositoryDaoBean dao = new UsersRepositoryDaoBean();
+        List<User> list = dao.getUsersList();
+
+        for (int i = 0; i < list.size() ; i++) {
+            System.out.println(list.get(i).getId());
+            System.out.println(list.get(i).getName());
+            System.out.println(list.get(i).getSurname());
+            System.out.println(list.get(i).getAge());
+            System.out.println(list.get(i).getLogin());
+            System.out.println("______________________");
+
+        }
+
+
+
+        /*System.out.println("Heloo World");
+        List<User> list =  UsersRepository.getRepository();
+        System.out.println(list.size());
+
+        for (int i = 0; i < list.size() ; i++) {
+
+            System.out.println(list.get(i).getId());
+            System.out.println(list.get(i).getName());
+            System.out.println(list.get(i).getSurname());
+            System.out.println(list.get(i).getAge());
+            System.out.println(list.get(i).getLogin());
+            System.out.println("______________________");
+
+        }*/
+
+
+
+
     }
 }
---
+++ src/main/java/com/infoshareacademy/searchengine/dao/UsersRepositoryDao.java
@@ -0,0 +1,16 @@
+package com.infoshareacademy.searchengine.dao;
+
+import com.infoshareacademy.searchengine.domain.User;
+
+import java.util.List;
+
+public interface UsersRepositoryDao {
+
+    public User addUser(User user);
+
+    public User getUserById(int id );
+
+    public User getUserByLogin(String login);
+
+    public List<User> getUsersList();
+}
